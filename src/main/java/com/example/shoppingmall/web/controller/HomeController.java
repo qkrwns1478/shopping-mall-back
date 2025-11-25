77a6adde2da3e1ccd8ec60ec0eed09b7e76f2fef
qkrwns1478 <qkrwns1478@gmail.com>
2025-11-25 22:03:35 +0900

    chore: redirect home to swagger docs

--- src/main/java/com/example/shoppingmall/web/controller/HomeController.java
+++ src/main/java/com/example/shoppingmall/web/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.shoppingmall.web.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "Hello World!";
+    public void home(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
     }
 }
