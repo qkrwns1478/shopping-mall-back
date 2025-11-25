51e8e3e5b2957e3a80443ae5a5be0d5122ce728b
qkrwns1478 <qkrwns1478@gmail.com>
2025-11-25 19:55:35 +0900

    chore: add vercel in allowed origins

--- src/main/java/com/example/shoppingmall/config/SecurityConfig.java
+++ src/main/java/com/example/shoppingmall/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://munsiksa.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/example/shoppingmall/config/WebMvcConfig.java
+++ src/main/java/com/example/shoppingmall/config/WebMvcConfig.java
@@ -21,7 +21,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://munsiksa.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
