eb3c1c8b427f9b1697f57fdba8a044fab0e76c76
qkrwns1478 <qkrwns1478@gmail.com>
2025-11-29 21:31:00 +0900

    feat: add DataInitializer

---
+++ src/main/java/com/example/shoppingmall/config/DataInitializer.java
@@ -0,0 +1,46 @@
+package com.example.shoppingmall.config;
+
+import com.example.shoppingmall.domain.Member;
+import com.example.shoppingmall.domain.MemberRole;
+import com.example.shoppingmall.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${adminEmail}")
+    private String adminEmail;
+
+    @Value("${adminPassword}")
+    private String adminPassword;
+
+    @Value("${adminAddress}")
+    private String adminAddress;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (memberRepository.findByEmail(adminEmail).isEmpty()) {
+            Member admin = new Member();
+            admin.setEmail(adminEmail);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
+            admin.setName("문식사");
+            admin.setAddress(adminAddress);
+            admin.setBirthday(LocalDate.now());
+            admin.setRole(MemberRole.ADMIN);
+            admin.setPoints(100000);
+
+            memberRepository.save(admin);
+            System.out.println("관리자 계정 생성 완료");
+        }
+    }
+}
